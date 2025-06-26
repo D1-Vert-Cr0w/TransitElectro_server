0089b6340e8bcd707f8b2a3ca50b58a1c4c83523
D1-Vert-Cr0w <91415369+D1-Vert-Cr0w@users.noreply.github.com>
2025-06-26 15:08:58 +0500

    connection changes

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    const a =  await mongoose.connect(`mongodb://127.0.0.1:27017`)
+    const a =  await mongoose.connect(`mongodb://AdminCherry:x1a2o3c4@127.0.0.1:27017/test`)
     .then(() => {console.log("mongodb connect")})
 }
 export default connectDB;
