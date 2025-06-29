e29160d5370b797b2b97e210f28763f4e042e7c8
D1-Vert-Cr0w <91415369+D1-Vert-Cr0w@users.noreply.github.com>
2025-06-29 21:24:21 +0500

    Https

--- server.js
+++ server.js
@@ -7,20 +7,37 @@ import productRoute from "./routes/colection.js";
 import categoriesRoute from "./routes/categories.js";
 import filtrRoute from "./routes/filtr.js";
 import subCategoriesRoute from "./routes/subcategories.js";
-const app = express();
-const port = 5000;
 app.use(express.json())
 //Подключение к Mongo
 app.use(cors());
 connectDB();
 connectCloudinary()
 
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// Чтение сертификата и ключа
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+// Роут для проверки
+app.get('/', (req, res) => {
+  res.send('Hello HTTPS World!');
+});
+
+// Запуск HTTPS-сервера
+const PORT = 443; // Стандартный порт для HTTPS
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server running on https://localhost:${PORT}`);
+});
 //Роуты
 app.use('/colection', productRoute);
 app.use('/filtr', filtrRoute);
 app.use('/categories', categoriesRoute);
 app.use('/subcategories', subCategoriesRoute);
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
